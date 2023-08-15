7133b9eda60dfce64e321ebfc1d29bf50b7e45f9
Isaac Vieira <isaacvf.dev@gmail.com>
2023-08-14 22:32:20 -0300

    [FEAT/10]: Added images to assets. Started the menu section, however, the size proportion is way too big

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,5 +1,23 @@
 <template>
-  <section>
-    <div class="text-amber-600 text-[54px] text-center font-normal">Menu</div>
+  <section class="w-full h-[1423px] bg-[url('assets/MenuBackground.png')] bg-slate-950  px-4 py-8 md:py-12 lg:py-16">
+    <h1 class="text-amber-600 text-[54px] text-center font-normal">Menu</h1>
+    <div id="menu-container" class="flex flex-row justify-evenly mt-20">
+      <div class="w-[553px] flex flex-col gap-2">
+        <img src="assets/menu-image-01.png" alt="Descrição da imagem">
+        <div class="flex justify-evenly gap-2">
+          <div class="flex flex-col gap-3">
+            <img src="assets/menu-image-02.png" alt="Descrição da imagem" >
+            <img src="assets/menu-image-03.png" alt="Descrição da imagem" >
+  
+          </div>
+          <div>
+            <img src="assets/menu-image-04.png" alt="Descrição da imagem">
+          </div>
+
+        </div>
+               
+      </div>
+      <div class="bg-cyan-600 w-[553px]">x</div>
+    </div>
   </section>
 </template>
