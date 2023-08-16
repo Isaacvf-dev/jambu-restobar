c56eab283cdece6ce2ce73b7c461244cadb62c03
Isaac Vieira <isaacvf.dev@gmail.com>
2023-08-15 23:52:48 -0300

    [FEAT/10]:Changes in layout menu.vue

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="w-full h-[1423px] bg-[url('assets/MenuBackground.png')] bg-slate-950  px-4 py-8 md:py-12 lg:py-16">
-    <h1 class="text-amber-600 text-[54px] text-center font-normal">Menu</h1>
-    <div id="menu-container" class="flex flex-row justify-evenly mt-20">
-      <div class="w-[553px] flex flex-col gap-2">
+  <section class="w-full h-auto bg-[url('assets/MenuBackground.png')] bg-slate-950  md:py-12 lg:py-10">
+    <h1 class="text-amber-600 text-5xl text-center font-normal">Menu</h1>
+    <div id="menu-container" class="flex flex-row justify-center  gap-20 mt-20">
+      <div class=" flex flex-col gap-2">
         <img src="assets/menu-image-01.png" alt="Descrição da imagem">
         <div class="flex justify-evenly gap-2">
           <div class="flex flex-col gap-3">
@@ -17,7 +17,17 @@
         </div>
                
       </div>
-      <div class="bg-cyan-600 w-[553px]">x</div>
+      <div class=" max-w-md bg-cyan-600 flex">
+        <div class="bg-slate-950">
+          <img src="assets/menu-item-01.png" alt="Descrição da imagem" class="">
+
+        </div>
+        <div>
+          <h3 class="text-xl">Bolinho de Costela</h3>
+          <p class="text-sm">São uma verdadeira tentação gastronômica para os amantes de sabores intensos e texturas irresistíveis.</p>
+        </div>
+       
+      </div>
     </div>
   </section>
 </template>
