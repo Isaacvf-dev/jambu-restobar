ef262fcd1e36b4d5da653cb8d4ace87c6d8afc44
Daniel Soares <daniel.soares@fortics.com.br>
2023-09-09 20:00:59 -0300

    feature(06): Add responsive in Testimonial.

--- components/Testimonial.vue
+++ components/Testimonial.vue
@@ -1,15 +1,15 @@
 <template>
   <section
     id="testimonial_section"
-    class="w-full h-[50vh] min-h-[550px] bg-cover bg-center bg-no-repeat bg-slate-950 bg-[url('assets/testimonial-background.png')] relative overflow-hidden"
+    class="w-full md:h-[50vh] md:min-h-[550px] bg-cover bg-center bg-no-repeat bg-slate-950 bg-[url('assets/testimonial-background.png')] relative overflow-hidden "
   >
     <div
       id="testimonial-container"
       class="flex items-center justify-between h-full p-6 max-w-7xl mx-auto"
     >
-      <div class="columns-3">
+      <div class="columns-3 md:flex hidden">
         <div
-          class="text-white card text-center flex flex-col items-center mr-4 h-auto"
+          class="text-white w-[434px] p-[35px] card text-center flex flex-col items-center mr-4 h-auto"
         >
           <div>
             <img class="perfil" src="assets/testimonial-01.png" alt="" />
@@ -31,7 +31,7 @@
         </div>
 
         <div
-          class="text-white card text-center flex flex-col items-center h-auto mr-4"
+          class="text-white w-[434px] p-[35px] card text-center flex flex-col items-center h-auto mr-4"
         >
           <div>
             <img class="perfil" src="assets/testimonial-02.png" alt="" />
@@ -54,7 +54,7 @@
         </div>
 
         <div
-          class="text-white card text-center flex flex-col items-center h-auto"
+          class="text-white w-[434px] p-[35px] card text-center flex flex-col items-center h-auto"
         >
           <div>
             <img class="perfil" src="assets/testimonial-03.png" alt="" />
@@ -74,16 +74,97 @@
           </p>
         </div>
       </div>
+
+      <div class="md:hidden w-11/12 mx-auto mt-8">
+        <Swiper
+          :modules="[SwiperAutoplay, SwiperNavigation]"
+          :slides-per-view="1"
+          :loop="true"
+          :autoplay="{
+            delay: 4000,
+            disableOnInteraction: true,
+          }"
+        >
+          <SwiperSlide>
+            <div
+              class="text-white card p-5 mt-20 text-center mr-4 w-full flex flex-col items-center"
+            >
+              <div>
+                <img class="perfil" src="assets/testimonial-01.png" alt="" />
+              </div>
+              <p class="font-bold text-xl">Ni Marques</p>
+              <div class="flex mb-4 mx-auto">
+                <img src="assets/star.svg" alt="" />
+                <img src="assets/star.svg" alt="" />
+                <img src="assets/star.svg" alt="" />
+                <img src="assets/star.svg" alt="" />
+                <img src="assets/star.svg" alt="" />
+              </div>
+              <p class="italic">
+                Espetacular! Sabe aquele lugar que você queria encontrar, comer
+                com vontade, lugar agradável, bonito, com ótimos drinks?! Pois
+                lá eu achei. Fiquei surpreso! O lugar é novo e superou minhas
+                expectativas. Recomendo, vou voltar e levar mais gente com
+                certeza!
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              class="text-white card p-5 mt-20 text-center flex flex-col items-center h-auto"
+            >
+              <div>
+                <img class="perfil" src="assets/testimonial-02.png" alt="" />
+              </div>
+              <p class="font-bold text-xl">Valter F</p>
+              <div class="flex mb-4">
+                <img src="assets/star.svg" alt="" />
+                <img src="assets/star.svg" alt="" />
+                <img src="assets/star.svg" alt="" />
+                <img src="assets/star.svg" alt="" />
+                <img src="assets/star.svg" alt="" />
+              </div>
+              <p class="italic">
+                Excelente atendimento, decoração de bom gosto, boa música e
+                ambiente aconchegante. Quanto aos pratos: melhor acarajé e abará
+                que já experimentei no Porto, além dos acompanhamentos que
+                estavam todos deliciosos, especialmente o camarão. Saí de lá já
+                pensando em voltar.
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              class="text-white card p-5 mt-20 text-center flex flex-col items-center h-auto"
+            >
+              <div>
+                <img class="perfil" src="assets/testimonial-03.png" alt="" />
+              </div>
+              <p class="font-bold text-xl">Rui P</p>
+              <div class="flex mb-4">
+                <img src="assets/star.svg" alt="" />
+                <img src="assets/star.svg" alt="" />
+                <img src="assets/star.svg" alt="" />
+                <img src="assets/star.svg" alt="" />
+                <img src="assets/star.svg" alt="" />
+              </div>
+              <p class="italic">
+                Lugar excelente, cheio de boa gente e um ambiente espetacular… a
+                comida foi simplesmente espetacular e diferente … Recomendo 100%
+                a experiencia do Jambu !
+              </p>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   </section>
 </template> 
 
 <style scoped>
 .card {
-  width: 434px;
   background: rgba(217, 217, 217, 0.49);
   border-radius: 20px;
-  padding: 35px;
 }
 
 .perfil {
