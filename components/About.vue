31705d23590a11d82236b9ee771dc522ece49e43
Isaac Vieira <isaacvf.dev@gmail.com>
2023-09-11 22:11:15 -0300

    [feat/07]: Added portuguese i18n

--- components/About.vue
+++ components/About.vue
@@ -9,22 +9,13 @@
     >
       <div class="text-white text-center font-poppins md:w-1/2 p-4 md:p-6 xl:p-20">
         <h1 class="md:text-5xl text-4xl mb-6 font-semibold md:mb-10">
-          Filosofia <span class="text-amber-600">&</span> Cozinha
+          {{ $t("aboutTitle1") }} <span class="text-amber-600">&</span> {{ $t("aboutTitle2") }}
         </h1>
         <p class="lg:text-lg text-base mb-2">
-          Bem-vindo ao saboroso encontro de culturas culinárias no coração do
-          Porto! No nosso restaurante, cada prato é uma viagem sensorial única,
-          onde ingredientes tradicionais e inspirações contemporâneas se fundem
-          em harmonia. Se você busca uma experiência gastronômica autêntica e
-          apaixonante, aqui é o seu destino.
+          {{ $t("aboutParagraph1") }}
         </p>
         <p class="lg:text-lg text-base">
-          Nossa dedicação à qualidade, aliada a um ambiente acolhedor e
-          descontraído, fará você se sentir em casa desde o primeiro momento.
-          Junte-se a nós e celebre o encontro de paladares e culturas em uma
-          experiência culinária que irá cativar seus sentidos e deixar você
-          ansioso para voltar. Venha desfrutar da nossa paixão pelo sabor e
-          tornar cada momento memorável.
+          {{ $t("aboutParagraph2") }}
         </p>
       </div>
       <div class="md:w-1/2 xl:columns-3 columns-2 w-5/6 mx-auto gap-2 my-4">
--- components/Menu.vue
+++ components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="menu_section" class="w-full flex flex-col justify-center p-6 pt-16 md:pb-10 md:pt-20  h-full md:h-screen bg-[url('assets/MenuBackground.png')] bg-slate-950">
-    <h1 class="text-amber-600 text-2xl sm:text-5xl text-center font-semibold mb-6 md:mb-10 lg:pt-6">Menu</h1>
+    <h1 class="text-amber-600 text-2xl sm:text-5xl text-center font-semibold mb-6 md:mb-10 lg:pt-6"> {{ $t('menuTitle') }} </h1>
     <div id="menu-container" class="w-full sm:flex flex-row justify-evenly lg:px-8 sm:max-h-[70vh]  pt-4 sm:pt-6 md:pb-4">
       <div class=" flex flex-col w-full max-w-3xl ">
         <NuxtImg src="assets/menu-image-01.png" loading="lazy" class="h-52 sm:h-2/5 object-cover pb-2" alt="Prato do menu" />
@@ -16,26 +16,26 @@
         <div class="max-w-md w-4/5 sm:w-fit m-auto md:mr-6 h-full flex flex-col gap-4 sm:justify-evenly pt-8 sm:pt-0 ">          
           <div class="relative h-1/5 lg:h-1/4 pl-24 pt-2 pb-4 pr-6 lg:pr-8  bg-slate-900 rounded-tr-md rounded-bl-md rounded-br-3xl">
             <NuxtImg src="assets/menu-item-01.png" loading="lazy" alt="Imagem do bolinho de costela" class="absolute top-0 left-0 w-2/6 lg:w-1/5 -ml-4 -mt-4" />          
-            <h3 class="text-sm md:text-lg lg:text-xl text-amber-600 pb-1">Bolinho de Costela</h3>
-            <p class="text-xs lg:text-base text-white">São uma verdadeira tentação gastronômica para os amantes de sabores intensos e texturas irresistíveis.</p>
+            <h3 class="text-sm md:text-lg lg:text-xl text-amber-600 pb-1"> {{ $t('menuItemTitle1') }} </h3>
+            <p class="text-xs lg:text-base text-white"> {{ $t('menuItemDescription1') }} </p>
           </div>
           <div class="relative h-1/5 lg:h-1/4 pl-24 pt-2 pb-4 pr-6 lg:pr-8 bg-slate-900 rounded-tr-md rounded-bl-md rounded-br-3xl">
             <NuxtImg src="assets/menu-item-02.png" loading="lazy" alt="Imagem do Mix Jambu" class="absolute top-0 left-0 w-2/6 lg:w-1/5 -ml-4 -mt-4"/>          
-            <h3 class="text-sm md:text-lg lg:text-xl text-amber-600 pb-1">Mix Jambu</h3>
-            <p class="text-xs lg:text-base text-white">O nosso mix com 3 tapas da casa, uma seleção irresistível que promete encantar.</p>
+            <h3 class="text-sm md:text-lg lg:text-xl text-amber-600 pb-1"> {{ $t('menuItemTitle2') }} </h3>
+            <p class="text-xs lg:text-base text-white"> {{ $t('menuItemDescription1') }} </p>
           </div>
           <div class="relative h-1/5 lg:h-1/4 pl-24 pt-2 pb-4 pr-6 lg:pr-8 bg-slate-900 rounded-tr-md rounded-bl-md rounded-br-3xl">
             <NuxtImg src="assets/menu-item-03.png" loading="lazy" alt="Imagem da porção de acarajé" class="absolute top-0 left-0 w-2/6 lg:w-1/5 -ml-4 -mt-4"/>          
-            <h3 class="text-sm md:text-lg lg:text-xl text-amber-600 pb-1">Porção de Acarajés</h3>
-            <p class="text-xs lg:text-base text-white">Seleção de 10 mini acarajés, uma iguaria brasileira cheia de sabor e tradição.</p>
+            <h3 class="text-sm md:text-lg lg:text-xl text-amber-600 pb-1"> {{ $t('menuItemTitle3') }} </h3>
+            <p class="text-xs lg:text-base text-white"> {{ $t('menuItemDescription3') }} </p>
           </div>
           <div class="relative h-1/5 lg:h-1/4 pl-24 pt-2 pb-4 pr-6 lg:pr-8 bg-slate-900 rounded-tr-md rounded-bl-md rounded-br-3xl">
             <NuxtImg src="assets/menu-item-04.png" loading="lazy" alt="Imagem do trio nordestino" class="absolute top-0 left-0 w-2/6 lg:w-1/5 -ml-4 -mt-4"/>          
-            <h3 class="text-sm md:text-lg lg:text-xl text-amber-600 pb-1">Trio Nordestino</h3>
-            <p class="text-xs lg:text-base text-white">A crocância das batatas fritas se funde com a carne seca e o sabor aconchegante do molho de queijos.</p>
+            <h3 class="text-sm md:text-lg lg:text-xl text-amber-600 pb-1"> {{ $t('menuItemTitle4') }} </h3>
+            <p class="text-xs lg:text-base text-white"> {{ $t('menuItemDescription4') }} </p>
           </div>        
                     
-          <button class="bg-amber-600 hover:bg-amber-700 transition duration-100 delay-75 text-xs sm:text-sm lg:text-base text-white w-fit pt-1 lg:p-2 pb-1 lg:px-6 px-2 rounded-3xl self-center font-semibold ">Menu Completo</button>
+          <button class="bg-amber-600 hover:bg-amber-700 transition duration-100 delay-75 text-xs sm:text-sm lg:text-base text-white w-fit pt-1 lg:p-2 pb-1 lg:px-6 px-2 rounded-3xl self-center font-semibold "> {{ $t('MenuButton') }} </button>
         </div>
        
       </div>
